8736f926111c978b95bbd43be9837572aaeb4f0f
Rangassamy <enzorangassamy@gmail.com>
2024-12-15 12:46:52 -0400

    first commit of the repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import React Router
+import Hero from "./Pages/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Définition des routes */}
+        <Routes>
+          <Route path="/" element={<Hero />} />{" "}
+          {/* Route pour la page d'accueil */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
