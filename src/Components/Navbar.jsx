cdd7ee52e43e96d103f4617a1231cecb926bdeaf
Rangassamy <enzorangassamy@gmail.com>
2024-12-28 12:26:26 -0400

    new version of Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,19 +5,15 @@ const Navbar = ({ page }) => {
     <div id="Navbar">
       <div className="leftpart">
         <img src="./img/logo-js.png" alt="logo javascript" />
-        <a href="/"><h1>Scriptify</h1></a>
+        <a href="/">
+          <h1>Scriptify</h1>
+        </a>
       </div>
 
       <ul className="rightpart">
-        <li>
-          
-        </li>
-        <li>
-          <a href="/Projects">Projects</a>
-        </li>
-        <li>
-          <a href="/Quizs">Quizs</a>
-        </li>
+        <a href="/Projects">Projects</a>
+
+        <a href="/Quizs">Quizs</a>
       </ul>
     </div>
   );
