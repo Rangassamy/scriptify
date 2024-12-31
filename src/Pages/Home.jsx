f5dbdfaa485701a3588c5964ff58d5dc235fcdf3
Rangassamy <enzorangassamy@gmail.com>
2024-12-31 09:23:56 -0400

    new version of Home without responsive and animation ( gsap )

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,44 +11,46 @@ const Home = () => {
           <div className="leftPart">
             <a href="/Quizs">Quizs</a>
           </div>
-          <div className="middlePart">
-            <div className="box">
-              <img id="arrow1" src="./img/arrow-img.png" alt="arrow-img" />
-            </div>
-            <div className="box">
-              <div className="circlePack1">
-                <div className="circle1"></div>
-                <div className="circle2"></div>
-                <div className="circle3"></div>
+          <div className="middlePartContainer">
+            <div className="middlePart">
+              <div className="box">
+                <i id="arrow1" class="fa-solid fa-chevron-left"></i>
               </div>
-              <div className="linePack2">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-                <div className="line4"></div>
-                <div className="line5"></div>
+              <div className="box">
+                <div className="circlePack1">
+                  <div className="circle1"></div>
+                  <div className="circle2"></div>
+                  <div className="circle3"></div>
+                </div>
+                <div className="linePack2">
+                  <div className="line1"></div>
+                  <div className="line2"></div>
+                  <div className="line3"></div>
+                  <div className="line4"></div>
+                  <div className="line5"></div>
+                </div>
               </div>
-            </div>
-            {/* <img src="./img/img-hero-page1.png" alt="js-book" /> */}
-            <div className="box">
-              <img id="img" src="./img/hero-img.png" alt="js-book" />
-            </div>
-            <div className="box">
-              <div className="linePack1">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-                <div className="line4"></div>
-                <div className="line5"></div>
+              {/* <img src="./img/img-hero-page1.png" alt="js-book" /> */}
+              <div className="box">
+                <img id="img" src="./img/hero-img.png" alt="js-book" />
               </div>
-              <div className="circlePack2">
-                <div className="circle1"></div>
-                <div className="circle2"></div>
-                <div className="circle3"></div>
+              <div className="box">
+                <div className="linePack1">
+                  <div className="line1"></div>
+                  <div className="line2"></div>
+                  <div className="line3"></div>
+                  <div className="line4"></div>
+                  <div className="line5"></div>
+                </div>
+                <div className="circlePack2">
+                  <div className="circle1"></div>
+                  <div className="circle2"></div>
+                  <div className="circle3"></div>
+                </div>
               </div>
-            </div>
-            <div className="box">
-              <img id="arrow2" src="./img/arrow-img.png" alt="arrow-img" />
+              <div className="box">
+                <i id="arrow2" class="fa-solid fa-chevron-right"></i>
+              </div>
             </div>
           </div>
           <div className="rightPart">
