38ae7dde52cbdfa1119cc7dbba071152de5971e0
Rangassamy <enzorangassamy@gmail.com>
2025-01-01 22:51:24 -0400

    first version of a little bit of animation with gsap in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavbarHome from "../Components/NavbarHome";
+import { gsap } from "gsap";
 
 const Home = () => {
- 
+  useEffect(() => {
+    gsap.to(".line", {
+      width: 30,
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+      stagger: 1,
+    });
+  }, []);
+
   return (
     <div>
       <NavbarHome />
@@ -18,34 +28,33 @@ const Home = () => {
               </div>
               <div className="box">
                 <div className="circlePack1">
-                  <div className="circle1"></div>
-                  <div className="circle2"></div>
-                  <div className="circle3"></div>
+                  <div className="circle" id="circle1"></div>
+                  <div className="circle" id="circle2"></div>
+                  <div className="circle" id="circle3"></div>
                 </div>
                 <div className="linePack2">
-                  <div className="line1"></div>
-                  <div className="line2"></div>
-                  <div className="line3"></div>
-                  <div className="line4"></div>
-                  <div className="line5"></div>
+                  <div className="line" id="line1"></div>
+                  <div className="line" id="line2"></div>
+                  <div className="line" id="line3"></div>
+                  <div className="line" id="line4"></div>
+                  <div className="line" id="line5"></div>
                 </div>
               </div>
-              {/* <img src="./img/img-hero-page1.png" alt="js-book" /> */}
               <div className="box">
                 <img id="img" src="./img/hero-img.png" alt="js-book" />
               </div>
               <div className="box">
                 <div className="linePack1">
-                  <div className="line1"></div>
-                  <div className="line2"></div>
-                  <div className="line3"></div>
-                  <div className="line4"></div>
-                  <div className="line5"></div>
+                  <div className="line" id="line1"></div>
+                  <div className="line" id="line2"></div>
+                  <div className="line" id="line3"></div>
+                  <div className="line" id="line4"></div>
+                  <div className="line" id="line5"></div>
                 </div>
                 <div className="circlePack2">
-                  <div className="circle1"></div>
-                  <div className="circle2"></div>
-                  <div className="circle3"></div>
+                  <div className="circle" id="circle1"></div>
+                  <div className="circle" id="circle2"></div>
+                  <div className="circle" id="circle3"></div>
                 </div>
               </div>
               <div className="box">
